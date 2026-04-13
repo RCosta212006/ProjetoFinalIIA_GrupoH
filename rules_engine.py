86a17694df1b666820a1d3573cf648e229c528ab
RCosta210606 <rodrigofcosta7@gmail.com>
2026-04-13 17:25:25 +0100

    Comentário com a definição de cada função.

--- rules_engine.py
+++ rules_engine.py
@@ -1,5 +1,11 @@
+# Este ficheiro vai:
+# - Definir regras;
+# - Aplicar regras ao dataset;
+# - Gerar riscos e acções;
+
 import pandas as pd
 
+# Função responsável pela verificação de dados inexistentes
 def has_values(row, columns):
     return all(pd.notna(row[col]) for col in columns)
 
@@ -11,6 +17,7 @@ def load_rules():
         ...
     ]
 
+
 def infer_row(row, rules):
     matched_rules = []
 
@@ -20,11 +27,14 @@ def infer_row(row, rules):
 
     return matched_rules
 
+# Decide qual o risco mais importante
 def highest_priority(rules):
     ...
 
+# Lê o CSV e aplica as regras a todas as linhas
 def process_dataset(file_path):
     ...
 
+# Corre o programa
 if __name__ == "__main__":
     ...
